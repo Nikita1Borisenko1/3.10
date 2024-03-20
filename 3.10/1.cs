7a34535830ace4f49216f54852fe9d364bbca0ed
ZKDS <nikitoc0@mail.ru>
2024-03-20 09:25:25 +0300

    1

---
+++ 3.10/1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace example_3_10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Введите число, которое нужно проверить на четность");
+            int number = int.Parse(ReadLine());
+            if (number % 2 == 0) WriteLine("Четное число");
+            else WriteLine("Число нечетное");
+            ReadKey();
+        }
+    }
+}
